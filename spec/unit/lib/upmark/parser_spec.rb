b0a37eeb000925ec44ac22c25a0501ab8435ae81
Josh Bassett <josh.bassett@gmail.com>
2011-09-22 20:30:36 +1000

    Cleanups.

--- spec/unit/lib/upmark/parser_spec.rb
+++ spec/unit/lib/upmark/parser_spec.rb
@@ -58,116 +58,128 @@ describe Upmark::Parser do
   context "#parse" do
     subject { parser.parse(html) }
 
-    context "<p>" do
-      context "with multiple inline elements" do
-        let(:html) { "<p>messenger</p><p>bag</p><p>skateboard</p>" }
+    context "single element" do
+      let(:html) { "<p>messenger</p>" }
 
-        it do
-          should == [
-            {
-              element: {
-                start_tag: {name: "p", attributes: []},
-                end_tag:   {name: "p"},
-                content:   [{text: "messenger"}]
-              }
-            }, {
-              element: {
-                start_tag: {name: "p", attributes: []},
-                end_tag:   {name: "p"},
-                content:   [{text: "bag"}]
-              }
-            }, {
-              element: {
-                start_tag: {name: "p", attributes: []},
-                end_tag:   {name: "p"},
-                content:   [{text: "skateboard"}]
-              }
+      it do
+        should == [
+          {
+            element: {
+              start_tag: {name: "p", attributes: []},
+              end_tag:   {name: "p"},
+              content:   [{text: "messenger"}]
             }
-          ]
-        end
-      end
-
-      context "with multiple elements" do
-        let(:html) { "<p>messenger</p>\n<p>bag</p>\n<p>skateboard</p>" }
-
-        it do
-          should == [
-            {
-              element: {
-                start_tag: {name: "p", attributes: []},
-                end_tag:   {name: "p"},
-                content:   [{text: "messenger"}]
-              }
-            }, {
-              text: "\n"
-            }, {
-              element: {
-                start_tag: {name: "p", attributes: []},
-                end_tag:   {name: "p"},
-                content:   [{text: "bag"}]
-              }
-            }, {
-              text: "\n"
-            }, {
-              element: {
-                start_tag: {name: "p", attributes: []},
-                end_tag:   {name: "p"},
-                content:   [{text: "skateboard"}]
-              }
-            }
-          ]
-        end
-      end
-
-      context "with nested elements" do
-        let(:html) { "<p>messenger <strong>bag</strong> skateboard</p>" }
-
-        it do
-          should == [
-            {
-              element: {
-                start_tag: {name: "p", attributes: []},
-                end_tag:   {name: "p"},
-                content:   [
-                  {text: "messenger "},
-                  {
-                    element: {
-                      start_tag: {name: "strong", attributes: []},
-                      content:   [{text: "bag"}],
-                      end_tag:   {name: "strong"}
-                    }
-                  }, {
-                    text: " skateboard"
-                  }
-                ]
-              }
-            }
-          ]
-        end
+          }
+        ]
       end
     end
 
-    context "<a>" do
-      context "single element" do
-        let(:html) { %q{<a href="http://helvetica.com/" title="art party organic">messenger bag skateboard</a>} }
+    context "single element with attributes" do
+      let(:html) { %q{<a href="http://helvetica.com/" title="art party organic">messenger bag skateboard</a>} }
 
-        it do
-          should == [
-            {
-              element: {
-                start_tag: {
-                  name: "a",
-                  attributes: [
-                    {name: "href",  value: "http://helvetica.com/"},
-                    {name: "title", value: "art party organic"}
-                  ]
-                },
-                end_tag: {name: "a"},
-                content: [{text: "messenger bag skateboard"}]
-              }
+      it do
+        should == [
+          {
+            element: {
+              start_tag: {
+                name: "a",
+                attributes: [
+                  {name: "href",  value: "http://helvetica.com/"},
+                  {name: "title", value: "art party organic"}
+                ]
+              },
+              end_tag: {name: "a"},
+              content: [{text: "messenger bag skateboard"}]
             }
-          ]
-        end
+          }
+        ]
+      end
+    end
+
+    context "multiple inline elements" do
+      let(:html) { "<p>messenger</p><p>bag</p><p>skateboard</p>" }
+
+      it do
+        should == [
+          {
+            element: {
+              start_tag: {name: "p", attributes: []},
+              end_tag:   {name: "p"},
+              content:   [{text: "messenger"}]
+            }
+          }, {
+            element: {
+              start_tag: {name: "p", attributes: []},
+              end_tag:   {name: "p"},
+              content:   [{text: "bag"}]
+            }
+          }, {
+            element: {
+              start_tag: {name: "p", attributes: []},
+              end_tag:   {name: "p"},
+              content:   [{text: "skateboard"}]
+            }
+          }
+        ]
+      end
+    end
+
+    context "multiple elements" do
+      let(:html) { "<p>messenger</p>\n<p>bag</p>\n<p>skateboard</p>" }
+
+      it do
+        should == [
+          {
+            element: {
+              start_tag: {name: "p", attributes: []},
+              end_tag:   {name: "p"},
+              content:   [{text: "messenger"}]
+            }
+          }, {
+            text: "\n"
+          }, {
+            element: {
+              start_tag: {name: "p", attributes: []},
+              end_tag:   {name: "p"},
+              content:   [{text: "bag"}]
+            }
+          }, {
+            text: "\n"
+          }, {
+            element: {
+              start_tag: {name: "p", attributes: []},
+              end_tag:   {name: "p"},
+              content:   [{text: "skateboard"}]
+            }
+          }
+        ]
+      end
+    end
+
+    context "nested elements" do
+      let(:html) { "<p>messenger <strong>bag</strong> skateboard</p>" }
+
+      it do
+        should == [
+          {
+            element: {
+              start_tag: {name: "p", attributes: []},
+              end_tag:   {name: "p"},
+              content:   [
+                {text: "messenger "},
+                {
+                  element: {
+                    start_tag: {name: "strong", attributes: []},
+                    content:   [{text: "bag"}],
+                    end_tag:   {name: "strong"}
+                  }
+                }, {
+                  text: " skateboard"
+                }
+              ]
+            }
+          }
+        ]
       end
     end
   end
